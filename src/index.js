bf6818cfee7db7e26d06d05d9d5d3117773305c7
Wei Junyu <weijunyu@users.noreply.github.com>
2019-08-01 12:47:22 +0800

    Changed sgds import to use sgds-govtech's npm package

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
 import * as serviceWorker from "./serviceWorker";
 
-import "./index.css";
+import "sgds-govtech/css/sgds.css";
+
 import App from "./App";
 
 const store = configureStore();
