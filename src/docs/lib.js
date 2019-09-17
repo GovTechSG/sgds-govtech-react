5a86315f2e31673f1f9da8c3f698d60120822b4e
Wei Junyu <weijunyu@users.noreply.github.com>
2019-09-17 11:47:32 +0800

    Removed unneeded components

--- src/docs/lib.js
+++
@@ -1,6 +0,0 @@
-const prettier = require("prettier/standalone");
-const plugins = [require("prettier/parser-babylon")];
-
-export function formatCode(code) {
-  return prettier.format(code, { parser: "babel", plugins });
-}
