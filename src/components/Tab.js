9ef64f83fc3238ed9cc6edb740fbdd0cb507fead
Chamil Udayanga <chamiludayanga28@gmail.com>
2019-10-14 15:28:20 +0800

    Fixed class to className, added key prop

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -4,29 +4,29 @@ import PropTypes from "prop-types";
 class Tab extends Component {
   constructor(props) {
     super(props);
-   this.classes = props.className
-   this.tabItems = props.tabItems
-   this.state = {
-     selectedTab:`tab0`
-   }
-  }
-  
-  handleTabClick(id){
-    this.setState({selectedTab:id})
+    this.classes = props.className
+    this.tabItems = props.tabItems
+    this.state = {
+      selectedTab: `tab0`
+    }
   }
 
-  renderTabs(){
-    let tabs = this.tabItems.map((item,idx)=>{
+  handleTabClick(id) {
+    this.setState({ selectedTab: id })
+  }
+
+  renderTabs() {
+    let tabs = this.tabItems.map((item, idx) => {
       let isSelected = this.state.selectedTab === `tab${idx}`
       let icon = []
-      if(item.icon){
-        icon = (<i class={`sgds-icon padding--right--sm ${item.icon}`}></i>)
+      if (item.icon) {
+        icon = (<i className={`sgds-icon padding--right--sm ${item.icon}`}></i>)
       }
       return (
-        <li key={`tab${idx}`} id={`tab${idx}`} class={isSelected?'is-active': ''}>
-          <a role="tab" aria-selected={isSelected} aria-controls={`tab${idx}`} onClick={()=>{this.handleTabClick(`tab${idx}`)}} >
-              {icon}
-              {item.title}
+        <li key={`tab${idx}`} id={`tab${idx}`} className={isSelected ? 'is-active' : ''}>
+          <a role="tab" aria-selected={isSelected} aria-controls={`tab${idx}`} onClick={() => { this.handleTabClick(`tab${idx}`) }} >
+            {icon}
+            {item.title}
           </a>
         </li>
       )
@@ -34,21 +34,21 @@ class Tab extends Component {
     return tabs
   }
 
-  renderTabContent(){
-    let content = this.tabItems.map((item,idx)=>{
+  renderTabContent() {
+    let content = this.tabItems.map((item, idx) => {
       let isSelected = this.state.selectedTab === `tab${idx}`
-      if(isSelected){
-        return(
-          <div class="row" id={`tab${idx}`} role="tabpanel" aria-labelledby={`tab${idx}`}>
-            <div class="col">
-                <div class="row">
-                    {item.content}
-                </div>
+      if (isSelected) {
+        return (
+          <div key={idx} className="row" id={`tab${idx}`} role="tabpanel" aria-labelledby={`tab${idx}`}>
+            <div className="col">
+              <div className="row">
+                {item.content}
+              </div>
             </div>
           </div>
         )
       }
-      return('')
+      return ('')
     })
     return content
   }
@@ -56,8 +56,8 @@ class Tab extends Component {
   render() {
     return (
       <div className={this.classes}>
-        <div class="sgds-tabs">
-          <ul class="no-margin" role="tablist">
+        <div className="sgds-tabs">
+          <ul className="no-margin" role="tablist">
             {this.renderTabs()}
           </ul>
         </div>
