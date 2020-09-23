87d658106fca2f997491f7195706aa0ff8115486
Wei Junyu <weijunyu@users.noreply.github.com>
2020-09-24 00:22:06 +0800

    Deleting extra files

--- .storybook.bak/addons.js
+++
@@ -1,4 +0,0 @@
-import "@storybook/addon-notes/register-panel";
-import "@storybook/addon-actions/register";
-import "@storybook/addon-links/register";
-import "@storybook/addon-a11y/register";
--- .storybook.bak/config.js
+++
@@ -1,9 +0,0 @@
-import { configure, addDecorator } from "@storybook/react";
-import { withA11y } from "@storybook/addon-a11y";
-// automatically import all files ending in *.stories.js
-const req = require.context("../stories", true, /\.stories\.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-addDecorator(withA11y);
-configure(loadStories, module);
