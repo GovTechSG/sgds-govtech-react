ebbf979a9a27559ef9cf976c432d3c65d932a962
clukhei <kheiboardwarrior@gmail.com>
2022-01-13 18:03:25 +0800

    Add Card Components, stories, UT

--- stories/helpers/index.ts
+++ stories/helpers/index.ts
@@ -1,27 +1,39 @@
 import BaseModal, { ModalProps as BaseModalProps } from '@restart/ui/Modal';
 
 export const commonInputPropsArgType = (modalProps: BaseModalProps) => {
-    const argTypes = Object.entries(modalProps).map(([key, value])=> {
-        //@ts-ignore
-        const defaultValue = value.default || null 
-        const propType = checkPropType(value.type)
-        const type = {name : propType}
+  const argTypes = Object.entries(modalProps).map(([key, value]) => {
+    //@ts-ignore
+    const defaultValue = value.default || null;
+    const propType = checkPropType(value.type);
+    const type = { name: propType };
 
-        return {[key]: {type, defaultValue, table: {defaultValue}}}
-
-    })
-    const argTypesObj = Object.assign({}, ...argTypes)
-    return argTypesObj
-}
+    return { [key]: { type, defaultValue, table: { defaultValue } } };
+  });
+  const argTypesObj = Object.assign({}, ...argTypes);
+  return argTypesObj;
+};
 
 const checkPropType = (type: any) => {
-    if (type === String) {
-        return 'string'
-    } 
-    if (type === Boolean)
-        return 'boolean'
-    
-    return 'object'
-}
+  if (type === String) {
+    return 'string';
+  }
+  if (type === Boolean) return 'boolean';
 
-export const HIDDEN_COMMON_PROPS = { bsPrefix: { table: { disable: true } } };
+  return 'object';
+};
+
+export const HIDDEN_COMMON_PROPS = {
+  bsPrefix: { table: { disable: true } },
+  as: { table: { disable: true } },
+};
+
+export const VARIANT = [
+  'primary',
+  'secondary',
+  'success',
+  'danger',
+  'warning',
+  'info',
+  'dark',
+  'light',
+];
