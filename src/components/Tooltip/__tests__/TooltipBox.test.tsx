83273b5a7cd72762134a7938e68c5c0880ee4df1
clukhei <kheiboardwarrior@gmail.com>
2022-01-20 11:10:27 +0800

    Add Tooltip unit test

--- src/components/Tooltip/__tests__/TooltipBox.test.tsx
+++ src/components/Tooltip/__tests__/TooltipBox.test.tsx
@@ -2,18 +2,18 @@ import { render } from '@testing-library/react';
 import TooltipBox from '../TooltipBox';
 import * as React from 'react';
 
-describe('Tooltip', () => {
+describe('TooltipBox', () => {
   it('Should output a tooltip with content', () => {
     const { getByTestId } = render(
-      <TooltipBox data-testid="test-tooltip" placement="right">
+      <TooltipBox data-testid="test-tooltip-box" placement="right">
         <strong>Tooltip Content</strong>
       </TooltipBox>
     );
 
-    expect(getByTestId('test-tooltip').classList).toContain('tooltip');
-    expect(getByTestId('test-tooltip').classList).toContain('bs-tooltip-end');
+    expect(getByTestId('test-tooltip-box').classList).toContain('tooltip');
+    expect(getByTestId('test-tooltip-box').classList).toContain('bs-tooltip-end');
 
-    expect(getByTestId('test-tooltip').getAttribute('x-placement')).toEqual(
+    expect(getByTestId('test-tooltip-box').getAttribute('x-placement')).toEqual(
       'right'
     );
   });
