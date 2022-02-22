a01c58c79f429a9be25b2624eda04b7bdca1186c
clukhei <kheiboardwarrior@gmail.com>
2022-02-22 22:50:14 +0800

    remove escapeRegex

--- src/components/Calendar/DatePicker.tsx
+++ src/components/Calendar/DatePicker.tsx
@@ -140,11 +140,11 @@ export const DatePicker: React.FC<DatePickerProps> = ({
       props.onChange('', '');
     }
   };
-  const escapeRegex = (str: string) => {
-    return String(str).replace(new RegExp(`[^0-9${SEPARATOR}]`), '');
-  };
+  // const escapeRegex = (str: string) => {
+  //   return String(str).replace(new RegExp(`[^0-9${SEPARATOR}]`), '');
+  // };
   const handleBadInput = (originalValue: string) => {
-    const parts = originalValue.replace(new RegExp(`[^0-9${escapeRegex(SEPARATOR)}]`), '')
+    const parts = originalValue.replace(new RegExp(`[^0-9${SEPARATOR}]`), '')
       .split(SEPARATOR);
     if (dateFormat.match(/MM.DD.YYYY/) || dateFormat.match(/DD.MM.YYYY/)) {
       if (parts[0] && parts[0].length > 2) {
