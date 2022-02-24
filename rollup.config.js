74ef3ca9f9cd8579bf3904d4954aa073f2d72d94
clukhei <kheiboardwarrior@gmail.com>
2022-02-24 23:13:32 +0800

    rollup config with preserved Modules in folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-
+import replace from '@rollup/plugin-replace';
+import rename from 'rollup-plugin-rename';
 const packageJson = require('./package.json');
 
 export default {
@@ -13,18 +14,22 @@ export default {
       format: 'cjs',
       sourcemap: true,
       exports: 'named',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
     },
     {
       dir: packageJson.module,
       format: 'esm',
       sourcemap: true,
       exports: 'named',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
     },
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({ useTsconfigDeclarationDir: true })
   ],
 };
