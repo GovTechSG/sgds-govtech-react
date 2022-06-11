10d8724ceb9532dbca3bc258613bfc5b975d4db7
clukhei <kheiboardwarrior@gmail.com>
2022-06-11 17:38:39 +0800

    fix(modal): Modaltitle element to be h3 by default

--- src/Modal/ModalTitle.tsx
+++ src/Modal/ModalTitle.tsx
@@ -1,6 +1,3 @@
 import createWithBsPrefix from '../utils/createWithBsPrefix';
-import divWithClassName from '../utils/divWithClassName';
 
-const DivStyledAsH4 = divWithClassName('h4');
-
-export default createWithBsPrefix('modal-title', { Component: DivStyledAsH4 });
+export default createWithBsPrefix('modal-title', { Component: 'h3' });
