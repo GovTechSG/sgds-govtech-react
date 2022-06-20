89fce39daa14ed001a40a796638075cb06836a21
clukhei <kheiboardwarrior@gmail.com>
2022-06-20 16:14:43 +0800

    docs(tabs, dropdown): update sb docs

--- src/Dropdown/DropdownMenu.tsx
+++ src/Dropdown/DropdownMenu.tsx
@@ -13,23 +13,64 @@ import warning from 'warning';
 import DropdownContext, { DropDirection } from './DropdownContext';
 import InputGroupContext from '../InputGroup/InputGroupContext';
 import NavbarContext from '../Nav/NavbarContext';
-import { AlignType, AlignDirection, alignPropType, Placement, ResponsiveAlignProp } from '../utils/types';
+import {
+  AlignType,
+  AlignDirection,
+  alignPropType,
+  Placement,
+  ResponsiveAlignProp,
+} from '../utils/types';
 import useWrappedRefWithWarning from '../utils/useWrappedRefWithWarning';
-import { BsPrefixProps, BsPrefixRefForwardingComponent } from '../utils/helpers';
+import {
+  BsPrefixProps,
+  BsPrefixRefForwardingComponent,
+} from '../utils/helpers';
 
 export type DropdownMenuVariant = 'dark' | string;
 
 export interface DropdownMenuProps
   extends BsPrefixProps,
     Omit<React.HTMLAttributes<HTMLElement>, 'onSelect'> {
+  /** Controls the visibility of the Dropdown menu  */
   show?: boolean;
+  /** Whether to render the dropdown menu in the DOM before the first time it is shown */
   renderOnMount?: boolean;
+  /** Have the dropdown switch to it's opposite placement when necessary to stay on screen. */
   flip?: boolean;
+  /**
+   * Aligns the dropdown menu to the specified side of the container. You can also align
+   * the menu responsively for breakpoints starting at `sm` and up. The alignment
+   * direction will affect the specified breakpoint or larger.
+   *
+   * *Note: Using responsive alignment will disable Popper usage for positioning.*
+   *
+   * @type {"start"|"end"|{ sm: "start"|"end" }|{ md: "start"|"end" }|{ lg: "start"|"end" }|{ xl: "start"|"end"}|{ xxl: "start"|"end"} }
+   */
   align?: AlignType;
+  /** Callback fired when selecting menu item
+   * `(eventKey: string | null, e: React.SyntheticEvent<unknown>) => void`
+   */
   onSelect?: SelectCallback;
+  /**
+   * Which event when fired outside the component will cause it to be closed
+   *
+   * *Note: For custom dropdown components, you will have to pass the
+   * `rootCloseEvent` to `<RootCloseWrapper>` in your custom dropdown menu
+   * component ([similarly to how it is implemented in `<Dropdown.Menu>`](https://github.com/react-bootstrap/react-bootstrap/blob/v0.31.5/src/DropdownMenu.js#L115-L119)).*
+   */
   rootCloseEvent?: 'click' | 'mousedown';
+  /**
+   * A set of popper options and props passed directly to Popper.
+   */
   popperConfig?: UseDropdownMenuOptions['popperConfig'];
+  /**
+   * Menu color variant.
+   *
+   * Omitting this will use the default light color.
+   */
   variant?: DropdownMenuVariant;
+
+  /** `@private` Provides context when used in NavDropdown component  */
   isNav?: boolean;
 }
 
@@ -90,7 +131,9 @@ const propTypes = {
    * Omitting this will use the default light color.
    */
   variant: PropTypes.string,
-  isNav: PropTypes.bool
+
+  /** @private Provides context when used in NavDropdown component  */
+  isNav: PropTypes.bool,
 };
 
 const defaultProps: Partial<DropdownMenuProps> = {
@@ -100,7 +143,7 @@ const defaultProps: Partial<DropdownMenuProps> = {
 export function getDropdownMenuPlacement(
   alignEnd: boolean,
   dropDirection?: DropDirection,
-  isRTL?: boolean,
+  isRTL?: boolean
 ) {
   const topStart = isRTL ? 'top-end' : 'top-start';
   const topEnd = isRTL ? 'top-start' : 'top-end';
@@ -120,118 +163,120 @@ export function getDropdownMenuPlacement(
   return placement;
 }
 
-export const DropdownMenu: BsPrefixRefForwardingComponent<'ul', DropdownMenuProps> =
-  React.forwardRef<HTMLElement, DropdownMenuProps>(
-    (
-      {
-        bsPrefix,
-        className,
-        align,
-        rootCloseEvent,
-        flip,
-        show: showProps,
-        renderOnMount,
-        // Need to define the default "as" during prop destructuring to be compatible with styled-components github.com/react-bootstrap/react-bootstrap/issues/3595
-        as: Component = 'ul',
-        popperConfig,
-        variant,
-        isNav,
-        ...props
-      },
-      ref,
-    ) => {
-      let alignEnd = false;
-      const isNavbar = useContext(NavbarContext);
-      const prefix = 'dropdown-menu'
-      const { align: contextAlign, drop, isRTL } = useContext(DropdownContext);
-      align = align || contextAlign;
-      const isInputGroup = useContext(InputGroupContext);
-
-      const alignClasses: string[] = [];
-      if (align) {
-        if (typeof align === 'object') {
-          const keys = Object.keys(align);
-
-          warning(
-            keys.length === 1,
-            'There should only be 1 breakpoint when passing an object to `align`',
-          );
-        
-          if (keys.length) {
-            const brkPoint = keys[0] as keyof ResponsiveAlignProp;
-            const direction: AlignDirection = align[brkPoint];
-
-            // .dropdown-menu-end is required for responsively aligning
-            // left in addition to align left classes.
-            alignEnd = direction === 'start';
-            alignClasses.push(`${prefix}-${brkPoint}-${direction}`);
-          }
-        } else if (align === 'end') {
-          alignEnd = true;
-        }
-      }
-
-      const placement = getDropdownMenuPlacement(alignEnd, drop, isRTL);
-
-      const [menuProps, { hasShown, popper, show, toggle }] = useDropdownMenu({
-        flip,
-        rootCloseEvent,
-        show: showProps,
-        usePopper: !isNavbar && alignClasses.length === 0,
-        offset: !isNav ? [0, 10] : undefined,
-        popperConfig,
-        placement,
-      });
-
-      menuProps.ref = useMergedRefs(
-        useWrappedRefWithWarning(ref, 'DropdownMenu'),
-        menuProps.ref,
-      );
-
-      useIsomorphicEffect(() => {
-        // Popper's initial position for the menu is incorrect when
-        // renderOnMount=true. Need to call update() to correct it.
-        if (show) popper?.update();
-      }, [show]);
-
-      if (!hasShown && !renderOnMount && !isInputGroup) return null;
-
-      // For custom components provide additional, non-DOM, props;
-      if (typeof Component !== 'string') {
-        menuProps.show = show;
-        menuProps.close = () => toggle?.(false);
-        menuProps.align = align;
-      }
-
-      let style = props.style;
-      if (popper?.placement) {
-        // we don't need the default popper style,
-        // menus are display: none when not shown.
-        style = { ...props.style, ...menuProps.style };
-        //@ts-ignore
-        props['x-placement'] = popper.placement;
-      }
-      return (
-        <Component
-          {...props}
-          {...menuProps}
-          style={style}
-          // Bootstrap css requires this data attrib to style responsive menus.
-          {...((alignClasses.length || isNavbar) && {
-            'data-bs-popper': 'static',
-          })}
-          className={classNames(
-            className,
-            prefix,
-            show && 'show',
-            alignEnd && `${prefix}-end`,
-            variant && `${prefix}-${variant}`,
-            ...alignClasses,
-          )}
-        />
-      );
+export const DropdownMenu: BsPrefixRefForwardingComponent<
+  'ul',
+  DropdownMenuProps
+> = React.forwardRef<HTMLElement, DropdownMenuProps>(
+  (
+    {
+      bsPrefix,
+      className,
+      align,
+      rootCloseEvent,
+      flip,
+      show: showProps,
+      renderOnMount,
+      // Need to define the default "as" during prop destructuring to be compatible with styled-components github.com/react-bootstrap/react-bootstrap/issues/3595
+      as: Component = 'ul',
+      popperConfig,
+      variant,
+      isNav,
+      ...props
     },
-  );
+    ref
+  ) => {
+    let alignEnd = false;
+    const isNavbar = useContext(NavbarContext);
+    const prefix = 'dropdown-menu';
+    const { align: contextAlign, drop, isRTL } = useContext(DropdownContext);
+    align = align || contextAlign;
+    const isInputGroup = useContext(InputGroupContext);
+
+    const alignClasses: string[] = [];
+    if (align) {
+      if (typeof align === 'object') {
+        const keys = Object.keys(align);
+
+        warning(
+          keys.length === 1,
+          'There should only be 1 breakpoint when passing an object to `align`'
+        );
+
+        if (keys.length) {
+          const brkPoint = keys[0] as keyof ResponsiveAlignProp;
+          const direction: AlignDirection = align[brkPoint];
+
+          // .dropdown-menu-end is required for responsively aligning
+          // left in addition to align left classes.
+          alignEnd = direction === 'start';
+          alignClasses.push(`${prefix}-${brkPoint}-${direction}`);
+        }
+      } else if (align === 'end') {
+        alignEnd = true;
+      }
+    }
+
+    const placement = getDropdownMenuPlacement(alignEnd, drop, isRTL);
+
+    const [menuProps, { hasShown, popper, show, toggle }] = useDropdownMenu({
+      flip,
+      rootCloseEvent,
+      show: showProps,
+      usePopper: !isNavbar && alignClasses.length === 0,
+      offset: !isNav ? [0, 10] : undefined,
+      popperConfig,
+      placement,
+    });
+
+    menuProps.ref = useMergedRefs(
+      useWrappedRefWithWarning(ref, 'DropdownMenu'),
+      menuProps.ref
+    );
+
+    useIsomorphicEffect(() => {
+      // Popper's initial position for the menu is incorrect when
+      // renderOnMount=true. Need to call update() to correct it.
+      if (show) popper?.update();
+    }, [show]);
+
+    if (!hasShown && !renderOnMount && !isInputGroup) return null;
+
+    // For custom components provide additional, non-DOM, props;
+    if (typeof Component !== 'string') {
+      menuProps.show = show;
+      menuProps.close = () => toggle?.(false);
+      menuProps.align = align;
+    }
+
+    let style = props.style;
+    if (popper?.placement) {
+      // we don't need the default popper style,
+      // menus are display: none when not shown.
+      style = { ...props.style, ...menuProps.style };
+      //@ts-ignore
+      props['x-placement'] = popper.placement;
+    }
+    return (
+      <Component
+        {...props}
+        {...menuProps}
+        style={style}
+        // Bootstrap css requires this data attrib to style responsive menus.
+        {...((alignClasses.length || isNavbar) && {
+          'data-bs-popper': 'static',
+        })}
+        className={classNames(
+          className,
+          prefix,
+          show && 'show',
+          alignEnd && `${prefix}-end`,
+          variant && `${prefix}-${variant}`,
+          ...alignClasses
+        )}
+      />
+    );
+  }
+);
 
 DropdownMenu.displayName = 'DropdownMenu';
 DropdownMenu.propTypes = propTypes;
