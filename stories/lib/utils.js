47673681fed87367374a20c9e1fcee19cf9cae74
zedang <ze.dang@thoughtworks.com>
2022-06-29 16:34:01 +0800

    fix: re-name prettier parser

--- stories/lib/utils.js
+++ stories/lib/utils.js
@@ -1,5 +1,5 @@
 const prettier = require("prettier/standalone");
-const plugins = [require("prettier/parser-babylon")];
+const plugins = [require("prettier/parser-babel")];
 
 export function formatCode(code) {
   return prettier.format(code, { parser: "babel", plugins });
