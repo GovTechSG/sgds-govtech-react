27bdbb721e78b3f7853d4c2425c8a1002a4902da
clukhei <kheiboardwarrior@gmail.com>
2023-04-12 00:31:37 +0800

    fix(safeFindDomNode): typescript errors affecting build

--- src/utils/safeFindDOMNode.ts
+++ src/utils/safeFindDOMNode.ts
@@ -4,7 +4,7 @@ export default function safeFindDOMNode(
   componentOrElement: React.ComponentClass | Element | null | undefined,
 ) {
   if (componentOrElement && 'setState' in componentOrElement) {
-    return ReactDOM.findDOMNode(componentOrElement);
+    return ReactDOM.findDOMNode(componentOrElement as any);
   }
   return (componentOrElement ?? null) as Element | Text | null;
 }
