911b2126dcde3df8f91d5b811cac83e4d09e988f
jmestxr <tan_xing_rong@hotmail.com>
2023-10-27 11:06:14 +0800

    test(table): update Table component in cypress apps to use sub-components

--- cypress/apps/next-app/app/page.tsx
+++ cypress/apps/next-app/app/page.tsx
@@ -2,9 +2,9 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
-import { Button, Row, Col, Container, Badge } from "@govtechsg/sgds-react"; 
+import { Button, Row, Col, Container, Badge, Table } from "@govtechsg/sgds-react";
 import { Nav, NavDropdown, Navbar } from '@govtechsg/sgds-react/Nav'
-import { Tabs, Tab} from '@govtechsg/sgds-react/Tabs'
+import { Tabs, Tab } from '@govtechsg/sgds-react/Tabs'
 import { CloseButton } from "@govtechsg/sgds-react/CloseButton";
 import { Alert } from "@govtechsg/sgds-react/Alert";
 import { TooltipBox, Tooltip } from "@govtechsg/sgds-react/Tooltip";
@@ -15,11 +15,11 @@ import { Modal } from "@govtechsg/sgds-react/Modal";
 import { Overlay } from "@govtechsg/sgds-react/Overlay";
 import { FileUpload } from "@govtechsg/sgds-react/FileUpload";
 import { Form } from '@govtechsg/sgds-react/Form'
-import {  Combobox } from "@govtechsg/sgds-react/Combobox";
+import { Combobox } from "@govtechsg/sgds-react/Combobox";
 import { useState, useRef } from "react";
 import { DatePicker } from "@govtechsg/sgds-react/DatePicker";
 
-const {Dropdown} = require('@govtechsg/sgds-react/Dropdown')
+const { Dropdown } = require('@govtechsg/sgds-react/Dropdown')
 
 const TabsCom = () => (
   <>
@@ -85,36 +85,36 @@ const TabsCom = () => (
       </Tab>
     </Tabs>
     <Tabs
-  defaultActiveKey="profile"
-  id="uncontrolled-tab-example3"
-  variant="tabs-info-toggle"
->
-  <Tab
-    eventKey="home"
-    title="Home"
-    contentLeft={<i className="bi bi-house"></i>}
-    contentBottom={99}
-  >
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco
-  </Tab>
-  <Tab
-    eventKey="profile"
-    title="Profile"
-    contentBottom={
-      <Badge bg="light" text="dark" className="right">
-        99
-      </Badge>
-    }
-  >
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has
-    roots in a piece of classical Latin literature from 45 BC, making it over
-    2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
-    College in Virginia, looked up one of the more obscure Latin words,
-    consectetur, from a Lorem Ipsum
-  </Tab>
-</Tabs>
+      defaultActiveKey="profile"
+      id="uncontrolled-tab-example3"
+      variant="tabs-info-toggle"
+    >
+      <Tab
+        eventKey="home"
+        title="Home"
+        contentLeft={<i className="bi bi-house"></i>}
+        contentBottom={99}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco
+      </Tab>
+      <Tab
+        eventKey="profile"
+        title="Profile"
+        contentBottom={
+          <Badge bg="light" text="dark" className="right">
+            99
+          </Badge>
+        }
+      >
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has
+        roots in a piece of classical Latin literature from 45 BC, making it over
+        2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+        College in Virginia, looked up one of the more obscure Latin words,
+        consectetur, from a Lorem Ipsum
+      </Tab>
+    </Tabs>
   </>
 );
 const NavbarCom = () => {
@@ -275,30 +275,30 @@ const Home: NextPage = () => {
   const handleShow = () => setModalShow(true);
 
   const [selectedFile, setSelectedFile] = useState({});
- const onChangeFile = (data: any) => {
+  const onChangeFile = (data: any) => {
     setSelectedFile(data);
   };
 
   return (
     <div className={styles.container}>
       <>
-      {/* <SgdsMasthead/> */}
-      <TabsCom/>  
-      <NavbarCom/>
-      <DatePicker
-  initialValue={new Date('2020-01-01')}
-  displayDate={new Date('2020-01-01')}
-/>
-      <Form>
-      <FileUpload
-        controlId="fileupload1"
-        onChangeFile={onChangeFile}
-        selectedFile={selectedFile}
-      >
-        <i className="bi bi-upload me-2"></i>Choose a file
-      </FileUpload>
-    </Form>
-      <Combobox menuList={["apple", "orange", "banana"]}></Combobox>
+        {/* <SgdsMasthead/> */}
+        <TabsCom />
+        <NavbarCom />
+        <DatePicker
+          initialValue={new Date('2020-01-01')}
+          displayDate={new Date('2020-01-01')}
+        />
+        <Form>
+          <FileUpload
+            controlId="fileupload1"
+            onChangeFile={onChangeFile}
+            selectedFile={selectedFile}
+          >
+            <i className="bi bi-upload me-2"></i>Choose a file
+          </FileUpload>
+        </Form>
+        <Combobox menuList={["apple", "orange", "banana"]}></Combobox>
         <Button variant="primary" onClick={handleShow}>
           modal
         </Button>
@@ -317,127 +317,156 @@ const Home: NextPage = () => {
           </Modal.Footer>
         </Modal>
       </>
-        <div>
-          <CloseButton></CloseButton>
-          <Dropdown>
-            <Dropdown.Toggle id="dropdownidomg">
-              Dropdown Button<i className="bi bi-chevron-down"></i>
-            </Dropdown.Toggle>
-            <Dropdown.Menu >
-              <Dropdown.Item href="#">Action</Dropdown.Item>
-              <Dropdown.Item href="#">Another action</Dropdown.Item>
-              <Dropdown.Item onClick={() => console.log("Clicked")}>
-                Something else
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <>
-            <Button ref={target} onClick={() => setShow(!show)}>
-              Click me!
-            </Button>
-            <Overlay target={target.current} show={show} placement="right">
-              {(props) => (
-                <TooltipBox id="overlay-example" {...props}>
-                  My Tooltip
-                </TooltipBox>
-              )}
-            </Overlay>
-          </>
-          <Tooltip type="click" content="im a tooltip">
-            <button>Tooltip</button>
-          </Tooltip>
-          <Tooltip type="hover" content="im a tooltip">
-            <button>Hover</button>
-          </Tooltip>
-        </div>
-        <SideNav>
-          <SideNav.Item eventKey="0">
-            <SideNav.Button>SideNav Item #1</SideNav.Button>
-            <SideNav.Collapse>
-              <>
-                <SideNav.Link eventKey="nl-1" href="#">
-                  number one
-                </SideNav.Link>
-                <SideNav.Link eventKey="nl-2" href="#">
-                  number one
-                </SideNav.Link>
-                <SideNav.Link eventKey="nl-3" href="#">
-                  number one
-                </SideNav.Link>
-              </>
-            </SideNav.Collapse>
-          </SideNav.Item>
-          <SideNav.Item eventKey="1">
-            <SideNav.Button>SideNav Item #2</SideNav.Button>
-            <SideNav.Collapse>
-              <>
-                <SideNav.Link eventKey="nl-4">number two</SideNav.Link>
-                <SideNav.Link eventKey="nl-6">number two</SideNav.Link>
-                <SideNav.Link eventKey="nl-7">number two</SideNav.Link>
-                <SideNav.Link eventKey="nl-8">number two</SideNav.Link>
-              </>
-            </SideNav.Collapse>
-          </SideNav.Item>
-          <SideNav.Item eventKey="2">
-            <SideNav.Button href="#">SideNav Item #3</SideNav.Button>
-          </SideNav.Item>
-        </SideNav>
-        {/* {<Tooltip content="this is tooltip"><div>Test</div></Tooltip>} */}
-        <Alert>Alert</Alert>
-        <Button>Button</Button>
-        <Toast>Toast</Toast>
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <div>
+        <CloseButton></CloseButton>
+        <Dropdown>
+          <Dropdown.Toggle id="dropdownidomg">
+            Dropdown Button<i className="bi bi-chevron-down"></i>
+          </Dropdown.Toggle>
+          <Dropdown.Menu >
+            <Dropdown.Item href="#">Action</Dropdown.Item>
+            <Dropdown.Item href="#">Another action</Dropdown.Item>
+            <Dropdown.Item onClick={() => console.log("Clicked")}>
+              Something else
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <>
+          <Button ref={target} onClick={() => setShow(!show)}>
+            Click me!
+          </Button>
+          <Overlay target={target.current} show={show} placement="right">
+            {(props) => (
+              <TooltipBox id="overlay-example" {...props}>
+                My Tooltip
+              </TooltipBox>
+            )}
+          </Overlay>
+        </>
+        <Tooltip type="click" content="im a tooltip">
+          <button>Tooltip</button>
+        </Tooltip>
+        <Tooltip type="hover" content="im a tooltip">
+          <button>Hover</button>
+        </Tooltip>
+      </div>
+      <SideNav>
+        <SideNav.Item eventKey="0">
+          <SideNav.Button>SideNav Item #1</SideNav.Button>
+          <SideNav.Collapse>
+            <>
+              <SideNav.Link eventKey="nl-1" href="#">
+                number one
+              </SideNav.Link>
+              <SideNav.Link eventKey="nl-2" href="#">
+                number one
+              </SideNav.Link>
+              <SideNav.Link eventKey="nl-3" href="#">
+                number one
+              </SideNav.Link>
+            </>
+          </SideNav.Collapse>
+        </SideNav.Item>
+        <SideNav.Item eventKey="1">
+          <SideNav.Button>SideNav Item #2</SideNav.Button>
+          <SideNav.Collapse>
+            <>
+              <SideNav.Link eventKey="nl-4">number two</SideNav.Link>
+              <SideNav.Link eventKey="nl-6">number two</SideNav.Link>
+              <SideNav.Link eventKey="nl-7">number two</SideNav.Link>
+              <SideNav.Link eventKey="nl-8">number two</SideNav.Link>
+            </>
+          </SideNav.Collapse>
+        </SideNav.Item>
+        <SideNav.Item eventKey="2">
+          <SideNav.Button href="#">SideNav Item #3</SideNav.Button>
+        </SideNav.Item>
+      </SideNav>
+      {/* {<Tooltip content="this is tooltip"><div>Test</div></Tooltip>} */}
+      <Alert>Alert</Alert>
+      <Button>Button</Button>
+      <Toast>Toast</Toast>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>#</Table.HeaderCell>
+            <Table.HeaderCell>First Name</Table.HeaderCell>
+            <Table.HeaderCell>Last Name</Table.HeaderCell>
+            <Table.HeaderCell>Username</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.DataCell>1</Table.DataCell>
+            <Table.DataCell>Mark</Table.DataCell>
+            <Table.DataCell>Otto</Table.DataCell>
+            <Table.DataCell>@mdo</Table.DataCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.DataCell>2</Table.DataCell>
+            <Table.DataCell>Jacob</Table.DataCell>
+            <Table.DataCell>Thornton</Table.DataCell>
+            <Table.DataCell>@fat</Table.DataCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.DataCell>3</Table.DataCell>
+            <Table.DataCell colSpan={2}>Larry the Bird</Table.DataCell>
+            <Table.DataCell>@twitter</Table.DataCell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      <p className={styles.description}>
+        Get started by editing{" "}
+        <code className={styles.code}>pages/index.tsx</code>
+      </p>
+      <div className={styles.grid}>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+          <h2>Documentation &rarr;</h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <a href="https://nextjs.org/learn" className={styles.card}>
+          <h2>Learn &rarr;</h2>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <a
+          href="https://github.com/vercel/next.js/tree/canary/examples"
+          className={styles.card}
+        >
+          <h2>Examples &rarr;</h2>
+          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h2>Deploy &rarr;</h2>
+          <p>
+            Instantly deploy your Next.js site to a public URL with Vercel.
+          </p>
+        </a>
+      </div>
     </div>
   );
 };
