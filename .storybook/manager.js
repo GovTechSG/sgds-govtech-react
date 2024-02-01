65768226a65cd2df54c05a5181b4fe679ccb54a7
clukhei <kheiboardwarrior@gmail.com>
2024-02-01 15:45:44 +0800

    chore(*): fix storybook theme by removing storybook-dark-mode addon

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,24 +5,6 @@ import sgdsTheme from './sgdsTheme';
 
 addons.setConfig({
   theme: sgdsTheme,
-  isFullscreen: false,
-  showNav: true,
-  showPanel: true,
   panelPosition: 'right',
   enableShortcuts: true,
-  isToolshown: true,
-  theme: undefined,
-  selectedPanel: undefined,
-  initialActive: 'sidebar',
-  sidebar: {
-    showRoots: false,
-    collapsedRoots: ['other'],
-  },
-  toolbar: {
-    title: { hidden: false, },
-    zoom: { hidden: false, },
-    eject: { hidden: false, },
-    copy: { hidden: false, },
-    fullscreen: { hidden: false, },
-  },
 });
