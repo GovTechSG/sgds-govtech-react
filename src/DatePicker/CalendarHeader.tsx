bb4a347b59b94a3ce74df89918c682a9e6bacfaf
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-02-02 16:01:04 +0800

    chore(datepicker): remove unused import

--- src/DatePicker/CalendarHeader.tsx
+++ src/DatePicker/CalendarHeader.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import DatePickerContext from './DatePickerContext';
-import { getTotalDaysInMonth } from './DatePicker';
 import { calculateYearRange } from './YearView';
 
 interface CalendarHeaderProps {
