b3588bf93c6f4ba524b031a9868bbfb7c985c9c7
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-02-29 13:56:15 +0800

    test(datepicker): fix existing test cases

--- tests/DatePicker/MonthView.test.tsx
+++ tests/DatePicker/MonthView.test.tsx
@@ -1,29 +1,55 @@
 import * as React from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import MonthView, {MONTH_LABELS} from '../../src/DatePicker/MonthView'
+import MonthView, { MONTH_LABELS } from '../../src/DatePicker/MonthView';
 
 describe('MonthView', () => {
-    const mockFn = jest.fn()
-    const displayDate = new Date()
-    it ('should have default html structure', ()=> {
-      
-        const {container} = render(<MonthView onClickMonth={mockFn} displayDate={displayDate} />)
+  const mockFn = jest.fn();
+  const mockhandleTabPressOnCalendarBody = jest.fn();
+  const mockOnChangeMonth = jest.fn();
+  const monthRefs = React.createRef<Array<HTMLButtonElement | null>>();
+  const selectedDate = new Date();
+  const displayDate = new Date();
+  it('should have default html structure', () => {
+    const { container } = render(
+      <MonthView
+        onClickMonth={mockFn}
+        selectedDate={selectedDate}
+        displayDate={displayDate}
+        show={true}
+        onChangeMonth={mockOnChangeMonth}
+        handleTabPressOnCalendarBody={mockhandleTabPressOnCalendarBody}
+        monthRefs={monthRefs}
+      />
+    );
 
-        expect(container.querySelector('.sgds.monthpicker')).toBeInTheDocument()
-        expect(container.querySelectorAll('button.month').length).toEqual(12)
-        expect(container.querySelectorAll('button.active.month').length).toEqual(1) 
-        expect(container.querySelector('button.active')?.textContent).toEqual(MONTH_LABELS[displayDate.getMonth()]) 
+    expect(container.querySelector('.sgds.monthpicker')).toBeInTheDocument();
+    expect(container.querySelectorAll('button.month').length).toEqual(12);
+    expect(
+      container.querySelectorAll('button.month.text-primary').length
+    ).toEqual(1);
+    expect(container.querySelector('button.text-primary')?.textContent).toEqual(
+      MONTH_LABELS[displayDate.getMonth()]
+    );
+  });
 
-    }) 
+  it('onClick handler should work', () => {
+    const { container } = render(
+      <MonthView
+        onClickMonth={mockFn}
+        selectedDate={selectedDate}
+        displayDate={displayDate}
+        show={true}
+        onChangeMonth={mockOnChangeMonth}
+        handleTabPressOnCalendarBody={mockhandleTabPressOnCalendarBody}
+        monthRefs={monthRefs}
+      />
+    );
+    expect(container.querySelectorAll('button').length).toEqual(12);
 
-    it('onClick handler should work', () => {
-        const { container} = render(<MonthView onClickMonth={mockFn} displayDate={displayDate} />)
-        expect(container.querySelectorAll('button').length).toEqual(12)
+    const buttonOne = container.querySelectorAll('button')[0];
 
-        const buttonOne = container.querySelectorAll('button')[0]
+    fireEvent.click(buttonOne);
 
-        fireEvent.click(buttonOne)
-
-        expect(mockFn).toHaveBeenCalled() 
-    })
-})
+    expect(mockFn).toHaveBeenCalled();
+  });
+});
