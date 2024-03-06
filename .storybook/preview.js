191f13d52de5c53ccc47dcbd2b3b5e24e44b968b
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-03-06 16:49:32 +0800

    style(*): poc using of tailwindcss

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../src/index.css';
 // import '@govtechsg/sgds/sgds/sgds.css'
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
