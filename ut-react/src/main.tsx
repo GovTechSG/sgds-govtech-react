55bb9e9d3d6c89628b0398b95fc243d4418ff3ee
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-06-04 10:28:28 +0800

    chore(*): remove libraries for ut

--- ut-react/src/main.tsx
+++ ut-react/src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import '@webcomponents/scoped-custom-element-registry';
-import '@govtechsg/sgds-web-component';
-import '@govtechsg/sgds/css/sgds.css';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
